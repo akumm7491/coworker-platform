8529a6df25c9fbf4a7df57e6930ab9064f91898b
Adam Kumm <adamk7491@gmail.com>
2024-11-26 00:09:04 -0700

    Identity service running on docker now.

--- backend/services/identity-service/src/config/typeorm.config.ts
+++ backend/services/identity-service/src/config/typeorm.config.ts
@@ -4,11 +4,14 @@ import { User } from '../domain/models/User';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.POSTGRES_URL,
-  host: process.env.NODE_ENV === 'test' ? 'localhost' : (process.env.DB_HOST || 'postgres'),
+  host: process.env.NODE_ENV === 'test' ? 'localhost' : process.env.DB_HOST || 'postgres',
   port: process.env.NODE_ENV === 'test' ? 5435 : parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.NODE_ENV === 'test' ? 'identity_service_test' : (process.env.DB_NAME || 'identitydb'),
+  database:
+    process.env.NODE_ENV === 'test'
+      ? 'identity_service_test'
+      : process.env.DB_NAME || 'identity_db',
   entities: [User],
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV === 'development',
